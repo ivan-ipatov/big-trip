24ec5e6ca783c044e291ab75dcd4a9e068d6d169
ivanipatov <ivanipatov12345@gmail.com>
2025-04-05 22:33:32 +0500

    Templatize that

--- src/view/formEdit.js
+++ src/view/formEdit.js
@@ -23,7 +23,7 @@ function createEditingFormTemplate(point) {
     })
     .join('');
   return `
- <li class="trip-events__item">
+            <li class="trip-events__item">
               <form class="event event--edit" action="#" method="post">
                 <header class="event__header">
                   <div class="event__type-wrapper">
